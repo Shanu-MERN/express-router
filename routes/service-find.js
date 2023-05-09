76d501df34c60d0bb7905d204d2a0c9208043cfb
Shanu Flash <shanuflash@gmail.com>
2023-05-09 08:50:48 +0530

    added service find

--- routes/service-find.js
+++ routes/service-find.js
@@ -6,4 +6,15 @@ route.get("/", (req, res) => {
   res.send("Choose Service:");
 });
 
+route.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const output = data.find((service) => service.id == id);
+  console.log(id);
+  if (output) {
+    res.send("The selected service is " + '"' + output.name + '"');
+  } else {
+    res.json("Invalid service ID");
+  }
+});
+
 export default route;
